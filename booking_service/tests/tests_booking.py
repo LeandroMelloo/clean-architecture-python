d88a697c821471da685caf1f2ae74e045e098804
LeandroMelloo <leandromoreno1909@hotmail.com>
2023-05-16 22:35:57 -0300

    Criando cenario de testes de aggregate

--- booking_service/tests/tests_booking.py
+++ booking_service/tests/tests_booking.py
@@ -1,10 +1,13 @@
 import unittest
 from datetime import datetime, timedelta
 import sys
+
 sys.path.append("..")
 sys.path.append("../../")
 from booking_service.domain.booking.entities import Booking
-from booking_service.domain.booking.exceptions import CheckinDateCannotBeAfterCheckoutDate
+from booking_service.domain.customers.entities import Customer
+from booking_service.domain.booking.exceptions import CheckinDateCannotBeAfterCheckoutDate, CustomerCannotBeBlank
+from booking_service.domain.customers.exceptions import InvalidCustomerDocumentException
 from booking_service.application.booking.booking_services import BookingService
 from booking_service.application.booking.booking_dto import BookingDto
 from booking_service.application.customers.customer_dto import CustomerDto
@@ -41,6 +44,29 @@ class BookingTests(unittest.TestCase):
         response = service.create_new_booking(booking_dto)
 
         self.assertEqual(response['code'], 'CHECKINAFTERCHECKOUT')
+
+    def test_customer_cannot_be_blank(self):
+        checkin = datetime.today()
+        checkout = datetime.today()
+        booking = Booking(checkin=checkin, checkout=checkout, customer=None)
+
+        self.assertRaises(CustomerCannotBeBlank, booking.is_valid)
+
+    def test_customer_must_have_valid_document(self):
+        checkin = datetime.today()
+        checkout = datetime.today()
+        customer = Customer("Customer", 18, "123", "a@a.com")
+        booking = Booking(checkin=checkin, checkout=checkout, customer=customer)
+
+        self.assertRaises(InvalidCustomerDocumentException, booking.is_valid)
+
+    def test_customer_happy_path(self):
+        checkin = datetime.today()
+        checkout = datetime.today()
+        customer = Customer("Customer", 18, "12356", "a@a.com")
+        booking = Booking(checkin=checkin, checkout=checkout, customer=customer)
+
+        self.assertTrue(booking.is_valid())
     
     def test_create(self):
         checkin = datetime.today()
